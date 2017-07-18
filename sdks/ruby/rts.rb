87a185c8587ef58def9cadc7b712891142e7db62
Shawn Anderson <shawn42@gmail.com>
2017-07-18 11:32:07 -0400

    ruby SDK now submits random moves for random units

--- sdks/ruby/rts.rb
+++ sdks/ruby/rts.rb
@@ -1,20 +1,33 @@
 require 'socket'
 require 'thread'
 require 'json'
+require 'set'
 
 class Game
+  MOVE_DIRECTIONS = ['N', 'S', 'W', 'E']
+  MOVE_COMMAND = 'MOVE'
   def process_updates_from_server(msgs)
     # msgs is an array of updates from the server
+    @units ||= Set.new
+    msgs.each do |msg|
+      msg['unit_updates'].each do |uu|
+        @units << uu['id'] if uu['type'] != 'base'
+      end
+    end
     # ...
     # return commands for your units
-    {commands: []}
+    {commands: [
+      {command: MOVE_COMMAND, unit: @units.to_a.sample, dir: MOVE_DIRECTIONS.sample},
+    ]}
   end
 end
 
 if $0 == __FILE__
+  port = ARGV[0] || 9090
   server = TCPServer.new port
   Thread.abort_on_exception = true
   loop do
+    puts "waiting for connection on #{port}"
     Thread.new(server.accept) do |server_connection|
       game = Game.new
       msg_from_server = Queue.new
